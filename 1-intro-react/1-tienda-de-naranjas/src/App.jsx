924b8d9f47707d00a4093d15a39753e2949f9e6e
marianssby <169182482+marianssby@users.noreply.github.com>
2024-10-23 07:40:22 -0500

    Add files via upload

---
+++ 1-intro-react/1-tienda-de-naranjas/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return(
+    <div>
+      <header>
+        <nav>Menu de navegacion</nav>
+      </header>
+      <section>Seccion promos</section>
+      <section>Seccion tienda</section>
+      <footer> Pie de pagina</footer>
+    </div>
+  )
+  
+}
+
+export default App
