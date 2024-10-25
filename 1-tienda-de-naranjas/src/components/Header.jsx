ec844bba9764da6097cf38fb267a842cdf296002
marianssby <169182482+marianssby@users.noreply.github.com>
2024-10-25 06:24:18 -0500

    Delete 1-tienda-de-naranjas directory

--- 1-tienda-de-naranjas/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function header() {
-  return (
-    <header>
-        <nav>Menu de navegacion</nav>
-     </header>
-  )
-}
--- 1-tienda-de-naranjas/src/components/Section.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function Section() {
-  return (
-    <section>Seccion promos</section>
-  )
-}
