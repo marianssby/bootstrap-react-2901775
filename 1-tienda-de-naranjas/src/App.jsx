b39e0f48b90957c06d7088360ee0a633fe73ab3b
marianssby <169182482+marianssby@users.noreply.github.com>
2024-10-25 06:19:25 -0500

    Add files via upload

---
+++ 1-tienda-de-naranjas/src/App.jsx
@@ -0,0 +1,17 @@
+ import {Header} from "components/Header"
+ import Section from "./components/Section"
+ import Article from "./components/Article"
+ import Footer from "./components/Footer"
+function App() {
+  return(
+    <div>
+    <Header />
+    <Section />
+    <Article />
+    <Footer />
+    </div>
+  )
+  
+}
+
+export default App
---
+++ 1-tienda-de-naranjas/src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+export default function header() {
+  return (
+    <header>
+        <nav>Menu de navegacion</nav>
+     </header>
+  )
+}
